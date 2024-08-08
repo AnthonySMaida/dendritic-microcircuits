c14397635322a3b6ee054bfe0b7fc069cdb395e5
Rémi Marseault <22910497+Larsluph@users.noreply.github.com>
2024-08-08 14:08:03 -0500

    Handle API errors in WebUI (#7)

--- static/script.js
+++ static/script.js
@@ -4,61 +4,72 @@ function cleanContainer() {
   Array.from(container.children).forEach(n => n.remove())
 }
 
+function handleApiData(json) {
+  cleanContainer()
+
+  const options = {
+    chart: {
+      height: 334,
+      width: 500,
+      type: 'line'
+    },
+    dataLabels: {
+      enabled: false
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+      },
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    xaxis: {
+      type: 'numeric'
+    },
+    yaxis: {
+      labels: {
+        formatter: val => val.toPrecision(2)
+      },
+      title: {
+        text: 'Loss'
+      }
+    }
+  }
+
+  for (const [title, data] of Object.entries(json)) {
+    if (!Array.isArray(data)) continue
+
+    const canvas = document.createElement('div')
+    container.appendChild(canvas)
+
+    const chart = new ApexCharts(canvas, {
+      ...options,
+      series: data.map(serie => ({ name: serie.title, data: serie.data })),
+      title: {
+        text: title,
+        align: 'center'
+      }
+    })
+    chart.render()
+  }
+}
+
+function catchApiErrors(body) {
+  cleanContainer()
+
+  const iframe = Object.assign(document.createElement('iframe'), { 'id': 'error' })
+  container.appendChild(iframe)
+
+  const iframeDoc = iframe.contentDocument || iframe.contentWindow.document
+  iframeDoc.body.innerHTML = body
+}
+
 function getData() {
-    /**
-     *  Fetch tells browser to send a request to the flask backend on the '/data' route.
-     *  What calls getData() ?
-     */
-  fetch('/data').then(r => r.json())
-    .then(json => {
-      cleanContainer()
-
-      const options = {
-        chart: {
-          height: 334,
-          width: 500,
-          type: 'line'
-        },
-        dataLabels: {
-          enabled: false
-        },
-        grid: {
-          row: {
-            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-            opacity: 0.5
-          },
-        },
-        stroke: {
-          curve: 'smooth'
-        },
-        xaxis: {
-          type: 'numeric'
-        },
-        yaxis: {
-          labels: {
-            formatter: val => val.toPrecision(2)
-          },
-          title: {
-            text: 'Loss'
-          }
-        }
-      }
-
-      for (const [title, data] of Object.entries(json)) {
-        if (!Array.isArray(data)) continue;
-
-        const canvas = document.createElement('div')
-        container.appendChild(canvas)
-
-        const chart = new ApexCharts(canvas, {
-          ...options,
-          series: data.map(serie => ({ name: serie.title, data: serie.data })),
-          title: {
-            text: title,
-            align: 'center'
-          }
-        })
-        chart.render()
-      }
-  })
+  /**
+   *  Fetch tells browser to send a request to the flask backend on the '/data' route.
+   *  What calls getData() ?
+   */
+  fetch('/data').then(r => r.ok ? r.json().then(handleApiData) : r.text().then(catchApiErrors))
 }
